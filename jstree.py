40dac2e884c1d46227a348c43130d5c3b7579b57
William Grim <william@grimapps.com>
2013-01-05 23:25:56 -0500

    Choosing a better name for the pretty printing function.

--- jstree.py
+++ jstree.py
@@ -2,14 +2,14 @@ import json
 import collections
 import os
 
-Node = collections.namedtuple('Node', ['name', 'children'])
+Node = collections.namedtuple('Node', ['data', 'children'])
 
 class JSTree(object):
   def __init__(self, paths):
     """
     >>> import jstree
     >>> paths = ["editor/2012-07/31/.classpath", "editor/2012-07/31/.project"]
-    >>> print jstree.JSTree(paths)
+    >>> print jstree.JSTree(paths).pretty()
     /
       editor/
         2012-07/
@@ -26,13 +26,15 @@ class JSTree(object):
         curr = curr.children[subpath]
     self._root = root
 
-  def _treeStr(self, root, depth):
+  def pretty(self, root=None, depth=0, spacing=2):
+    if root is None:
+      root = self._root
     fmt = "%s%s/" if root.children else "%s%s"
-    s = fmt % (" "*depth*2, root.name)
+    s = fmt % (" "*depth*spacing, root.data)
     for child in root.children:
       child = root.children[child]
-      s += "\n%s" % self._treeStr(child, depth+1)
+      s += "\n%s" % self.pretty(child, depth+1, spacing)
     return s
   
   def __str__(self):
-    return self._treeStr(self._root, 0)
+    return str(self._root)
