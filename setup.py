6f944615c2cb0c3d9954ee82539b398102ca9580
William Grim <william@grimapps.com>
2013-01-07 00:08:45 -0500

    Fixing the setup.py for older versions of python.

--- setup.py
+++ setup.py
@@ -2,12 +2,19 @@ from setuptools import setup
 import os
 
 def read(filename):
-  with open(filename) as fin:
-    return fin.read()
+  fin = None
+  data = None
+  try:
+    fin = open(filename)
+    data = fin.read()
+  finally:
+    if fin is not None:
+      fin.close()
+  return data
 
 setup(
   name='jstree',
-  version='0.1',
+  version='0.1.1',
   author='William Grim',
   author_email='william@grimapps.com',
   url='https://github.com/grimwm/py-jstree',
